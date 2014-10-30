94dc00da984866b745b367bd0a80430b198ffa16
ClumsyCoder <robert.burnfield@gmail.com>
2014-10-29 21:51:18 -0400

    init for the HMVC Plugin

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WordPress HMVC Framework
+ * Plugin URI: http://github.com/ClumsyCoder/WordPress-HMVC-Framework
+ * Description: An HMVC framework for building plugins and themes
+ * Author: Robert Burnfield
+ * Author URI: http://github.com/ClumsyCoder
+ */
+
+require( __DIR__ . '/vendor/autoload.php' );
